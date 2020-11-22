085dd896f4509b9a166b17a5893ca155b7ba07a9
jungwoonlim <wypo23@naver.com>
2020-11-23 00:03:05 +0900

    Apollo Sample Data

--- src/apollo.js
+++ src/apollo.js
@@ -1,5 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+// uri: "https://movieql.now.sh/",
 const client = new ApolloClient({
   uri: "https://48p1r2roz4.sse.codesandbox.io",
   cache: new InMemoryCache(),
--- src/routers/Home.js
+++ src/routers/Home.js
@@ -1,3 +1,26 @@
-import React from "react";
+import { gql, useQuery } from "@apollo/client";
 
-export default () => <h1>Home</h1>;
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`;
+
+function Home() {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+  if (loading) return <p>Loading....</p>;
+  if (error) return <p>Error...:(</p>;
+
+  return data.rates.map(({ currency, rate }) => (
+    <div key={currency}>
+      <h1>
+        {currency}: {rate}
+      </h1>
+    </div>
+  ));
+}
+
+export default Home;
